adef6566fbc0f6d369a2d2f8a32dfc71e4734f08
Konrad Pabjan <kopabjan@microsoft.com>
2020-05-07 12:56:18 +0200

    testing

--- dist/index.js
+++ dist/index.js
@@ -6330,8 +6330,8 @@ function installPython(workingDirectory) {
             yield exec.exec('powershell', ['./setup.ps1'], options);
         }
         else {
-            yield exec.exec('sudo bash', ['chmod +x ./setup.sh'], options);
-            yield exec.exec('sudo bash', ['-E ./setup.sh'], options);
+            yield exec.exec('bash', ['chmod +x ./setup.sh'], options);
+            yield exec.exec('bash', ['sudo -E ./setup.sh'], options);
         }
     });
 }
